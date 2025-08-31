d9df46bf745531523194cf052a749e501a58fabe
Samxarifa <samuelhay9@gmail.com>
2025-08-31 17:19:01 +0100

    Added Cooldown

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import PlaystationAPI from "./playstation";
 
 const API = new PlaystationAPI(process.env.NPSSO!, process.env.GROUP_ID!);
 
+let cooldown: number | undefined;
+
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -17,7 +19,15 @@ client.once(Events.ClientReady, c => {
 });
 
 client.on(Events.VoiceStateUpdate, (_, newState) => {
-    if (newState.member && newState.channel?.members.size === 1) {
-        API.sendMessage(`<${newState.member?.displayName} has created a discord party in ${newState.guild?.name}>`);
+    if (newState.channel?.members.size === 1) {
+        if (cooldown && Date.now() < cooldown) {
+            console.log("Hit Cooldown");
+            return;
+        }
+        const message = `${newState.member?.displayName} has created a discord party in ${newState.guild?.name}`;
+
+        console.log(message);
+        API.sendMessage(`<${message}>`);
+        cooldown = Date.now() + Number(process.env.COOLDOWN) * 60_000;
     }
 });
